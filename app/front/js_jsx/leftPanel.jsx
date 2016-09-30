57a89ea04e50702ec4fac183c5a7e1caca00de2b
ArguelBenoit <benoit.arguel@gmail.com>
2016-09-30 16:10:43 +0200

    feat: adding leftPanelSpot

--- app/front/js_jsx/leftPanel.jsx
+++ app/front/js_jsx/leftPanel.jsx
@@ -1,15 +1,18 @@
 import React, { PropTypes } from 'react';
-import LeftPanelSpot from './leftPanelSpot.jsx'
+import LeftPanelSpot from './leftPanelSpot.jsx';
 
 function LeftPanel(props) {
   const { detail, place, allId } = props.state;
   return <div className="elements-ui-absolute" id="left-panel" >
-    {allId.map((item, i) => <LeftPanelSpot index={i} {...item} />)}
+    {allId.map((item, i) => <LeftPanelSpot key={i} index={i} detail={detail[allId[i]]} place={place[allId[i]]} {...item} />)}
   </div>;
 }
 
 LeftPanel.propTypes = {
-  state: PropTypes.object
+  state: PropTypes.object,
+  detail: PropTypes.object,
+  place: PropTypes.object,
+  allId: PropTypes.array
 };
 
 export default LeftPanel;
--- app/front/js_jsx/leftPanelSpot.jsx
+++ app/front/js_jsx/leftPanelSpot.jsx
@@ -1,12 +1,42 @@
 import React, { PropTypes } from 'react';
+import { unitType, windColor } from './config.js';
 
-function LeftPanelSpot() {
-  return <div>
+function LeftPanelSpot(props) {
+  const { index, place, detail } = props;
+  var firstDetail = detail[0],
+      detailSplited = firstDetail.split(' '),
+      average = detailSplited[3],
+      heading = -detailSplited[5];
+  const color = windColor[Math.round(unitType(average))];
+
+  const styleChild = {
+    color: '#DDD'
+  };
+  const styleSpanCity = {
+    fontStyle: 'italic'
+  };
+  const styleSpanAverage = {
+    color: color,
+    float: 'right',
+    marginRight: '8px'
+  };
+  const styleImgAverage = {
+    transform: 'rotateZ(' + heading + 'deg)',
+    float: 'right',
+    fontWeight: 'bold'
+  };
+
+  return <div style={styleChild} className="child-panel button">
+    <span style={styleSpanCity}>{'Anémomètre - ' + place[0]}</span>
+    <img style={styleImgAverage} src="img/windheading.png" width="20px" height="20px" />
+    <span style={styleSpanAverage}>{unitType(average) + ' kn'}</span>
   </div>;
 }
 
-// LeftPanel.propTypes = {
-//   state: PropTypes.object
-// };
+LeftPanelSpot.propTypes = {
+  index: PropTypes.number,
+  detail: PropTypes.array,
+  place: PropTypes.array
+};
 
 export default LeftPanelSpot;

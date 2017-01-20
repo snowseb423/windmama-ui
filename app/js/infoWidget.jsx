0d1f5af43f240dc630e6b7569e129db60192690a
BenoitArguel <benoit.arguel@gmail.com>
2017-01-20 17:29:45 +0100

    refactor: refact infowidget, remove store import and add props

--- app/js/infoWidget.jsx
+++ app/js/infoWidget.jsx
@@ -1,43 +1,36 @@
-import React, { PropTypes, Component } from 'react';
+import React, { PropTypes } from 'react';
 import { Actions } from './store/actions.js';
-import store from './store/store.js';
 
-
-class InfoWidget extends Component {
-  constructor(props) {
-    super(props);
-  }
-  componentDidMount() {}
-  componentWillUnmount() {}
-  render() {
-    const { idStation } = this.props;
-    const styleCityName = {
-      fontFamily: 'Abel',
-      fontWeight: 'bolder',
-      fontSize: '18px',
-      padding: '0 10px'
-    };
-    if(idStation) {
-      var info = store.place[idStation].split('|')[4] + ', '+ 'Pioupiou: ' + store.place[idStation].split('|')[0];
-      if (info.indexOf('Unnamed Road') >= 0) {
-        info = info.substring(13);
-      }
+function InfoWidget(props) {
+  const { place } = props;
+  const styleCityName = {
+    fontFamily: 'Abel',
+    fontWeight: 'bolder',
+    fontSize: '18px',
+    padding: '0 10px'
+  };
+  var id;
+  if(place) {
+    id = place.split('|')[0];
+    var info = place.split('|')[4] + ', '+ 'Pioupiou: ' + id;
+    if (info.indexOf('Unnamed Road') >= 0) {
+      info = info.substring(13);
     }
-    return <div className="widget" style={{ padding: '10px 0', background: 'rgba(255, 255, 255, 0.25)', color: 'black'}}>
-      <div style={{ display: 'inline-block', width: '100%' }}>
-        <button className="button" style={{ float: 'left', marginLeft: '10px'}} onClick={() => Actions.addBookmark(idStation)}><i className="fa fa-plus-circle" aria-hidden="true"/> favoris</button>
-        <button className="button"  style={{ float: 'right', marginRight: '10px'}} onClick={() => Actions.displayDetail(false)}><i className="fa fa-times" /></button>
-        <button className="button"  style={{ float: 'right', marginRight: '10px'}} onClick={() => Actions.rightActivation()}> <i className="fa fa-info-circle" /></button>
-      </div>
-      <div style={styleCityName}>
-        {idStation ? info : ' '}
-      </div>
-    </div>;
   }
+  return <div className="widget" style={{ padding: '10px 0', background: 'rgba(255, 255, 255, 0.25)', color: 'black'}}>
+    <div style={{ display: 'inline-block', width: '100%' }}>
+      <button className="button" style={{ float: 'left', marginLeft: '10px'}} onClick={() => Actions.addBookmark(id)}><i className="fa fa-plus-circle" aria-hidden="true"/> favoris</button>
+      <button className="button"  style={{ float: 'right', marginRight: '10px'}} onClick={() => Actions.displayDetail(false)}><i className="fa fa-times" /></button>
+      <button className="button"  style={{ float: 'right', marginRight: '10px'}} onClick={() => Actions.rightActivation()}> <i className="fa fa-info-circle" /></button>
+    </div>
+    <div style={styleCityName}>
+      {id ? info : ' '}
+    </div>
+  </div>;
 }
 
 InfoWidget.propTypes = {
-  idStation: PropTypes.any
+  place: PropTypes.substring
 };
 
 export default InfoWidget;

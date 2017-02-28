e9513a35e17f06c33a8393b70de08c30c065584f
BenoitArguel <benoit.arguel@gmail.com>
2017-02-28 11:09:47 +0100

    update other pc

--- app/js/header.jsx
+++ app/js/header.jsx
@@ -3,15 +3,18 @@ import { Actions } from './store/actions.js';
 
 
 function Header(props) {
-  return <div id="header" className="elements-ui-absolute">
-    <div className={props.leftActive ? 'container-left-menu button clicked' : 'container-left-menu button'} onClick={() => Actions.leftActivation()}>
-      <i className="fa fa-arrow-down" aria-hidden="true" />
+  return <div>
+    <div id="header" className="elements-ui-absolute">
+      <div className={props.leftActive ? 'container-left-menu button clicked' : 'container-left-menu button'} onClick={() => Actions.leftActivation()}>
+        <i className="fa fa-arrow-down" aria-hidden="true" />
+      </div>
+      <h1 style={{cursor: 'pointer'}} onClick={() => Actions.displayDetail(false)}>WindMama.fr</h1>
+      <div className={props.rightActive ? 'container-right-menu button clicked' : 'container-right-menu button'} onClick={() => Actions.rightActivation()}>
+        <div/>
+        <div/>
+      </div>
     </div>
-    <h1 style={{cursor: 'pointer'}} onClick={() => Actions.displayDetail(false)}>WindMama.fr</h1>
-    <div className={props.rightActive ? 'container-right-menu button clicked' : 'container-right-menu button'} onClick={() => Actions.rightActivation()}>
-      <div/>
-      <div/>
-    </div>
+    <div style={{width: '100%', height: '60px'}} />
   </div>;
 }
 

e7e50e2e652ef0a770768ebf42041f241f276e38
BenoitArguel <benoit.arguel@gmail.com>
2017-03-13 16:56:02 +0100

    update other pcx

--- app/js/mapContainer.jsx
+++ app/js/mapContainer.jsx
@@ -1,34 +1,39 @@
-var assign = require('object-assign');
-var MapGL = require('react-map-gl');
-var React = require('react');
+import MapGL, { HTMLOverlay } from 'react-map-gl';
+import React, { Component, PropTypes } from 'react';
 
-var MapContainer = React.createClass({
-  getInitialState() {
-    return {
+
+class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       viewport: {
-        latitude: 37.78,
-        longitude: -122.45,
-        zoom: 11,
-        width: 800,
-        height: 800,
+        latitude: this.props.mapPosition[1],
+        longitude: this.props.mapPosition[0],
+        zoom: 5,
+        width: '100%',
+        height: '100%',
         startDragLngLat: null,
         isDragging: null
       }
     };
-  },
-
-  _onChangeViewport(newViewport) {
-    this.setState({viewport: newViewport});
-  },
-
-  render() {
-    var {mapStyle, viewport} = this.state;
-    return <MapGL
-      onChangeViewport={this._onChangeViewport}
-      {...viewport}
-      mapboxApiAccessToken={'pk.eyJ1IjoiYXJndWVsYmVub2l0IiwiYSI6ImNpczN0aTRpbjAwMWQyb3FkM3d4d3dweWwifQ.TuZpfqS-HyuaUzbe1fIiTg'}
-    />;
+    this._onChangeViewport = this._onChangeViewport.bind(this);
   }
-});
+  _onChangeViewport(newViewport) {
+    var viewport = Object.assign({}, this.state.viewport, newViewport);
+    this.setState({viewport});
+  }
+  render() {
+    var loc = [-122.39851786165565, 37.78736425435588];
+    var { viewport } = this.state;
+    return <MapGL onChangeViewport={this._onChangeViewport} {...viewport} mapboxApiAccessToken={'pk.eyJ1IjoiYXJndWVsYmVub2l0IiwiYSI6ImNpczN0aTRpbjAwMWQyb3FkM3d4d3dweWwifQ.TuZpfqS-HyuaUzbe1fIiTg'} >
+      <HTMLOverlay width={viewport.width} height={viewport.height}/>
+    </ MapGL>;
+  }
+}
 
-module.exports = MapContainer;
+MapContainer.propTypes = {
+  viewport: PropTypes.any,
+  mapPosition: PropTypes.array
+};
+
+export default MapContainer;

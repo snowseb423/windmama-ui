1ff5de13651f7da1ebc572ea58c97865f9046f93
BenoitArguel <benoit.arguel@gmail.com>
2017-03-13 17:45:50 +0100

    update other pc

--- app/js/mapContainer.jsx
+++ app/js/mapContainer.jsx
@@ -1,39 +1,68 @@
-import MapGL, { HTMLOverlay } from 'react-map-gl';
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+import { Actions } from './store/actions.js';
+import { windColor } from './common.js';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJndWVsYmVub2l0IiwiYSI6ImNpczN0aTRpbjAwMWQyb3FkM3d4d3dweWwifQ.TuZpfqS-HyuaUzbe1fIiTg';
 
 class MapContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      viewport: {
-        latitude: this.props.mapPosition[1],
-        longitude: this.props.mapPosition[0],
-        zoom: 5,
-        width: '100%',
-        height: '100%',
-        startDragLngLat: null,
-        isDragging: null
-      }
-    };
-    this._onChangeViewport = this._onChangeViewport.bind(this);
   }
-  _onChangeViewport(newViewport) {
-    var viewport = Object.assign({}, this.state.viewport, newViewport);
-    this.setState({viewport});
+  componentDidMount() {
+    this.mymap = new mapboxgl.Map({
+      style: 'mapbox://styles/arguelbenoit/cixuf036e00632rqn6tcxdbuu',
+      container: 'map',
+      center: this.props.mapPosition,
+      zoom: 5
+    });
+    this.mymap.dragRotate.disable();
+    this.mymap.touchZoomRotate.disableRotation();
+    this.mymap.once('load', () => {
+      this.element();
+    });
+  }
+  element() {
+    this.props.allId.forEach((element) => {
+      if (typeof this.props.detail[element] !== 'undefined') {
+        var detailSplited = this.props.detail[element][0].split('|');
+        var placeSplited = this.props.place[element].split('|');
+
+        var htmlMarker = document.createElement('div');
+        htmlMarker.className = 'marker marker_' + element;
+
+        htmlMarker.innerHTML =  '<div class="child-marker child-marker-1" style="background: ' + windColor[Math.round((detailSplited[4]/1.852))] + ';"></div>' +
+        '<div class="child-marker child-marker-2" style="background: ' + windColor[Math.round((detailSplited[4]/1.852))] + ';"></div>' +
+        '<img src="img/windheading2.png" width="100%" height="100%" class="child-marker child-marker-3" style="transform: scale(0.5) rotateZ(' + Number(detailSplited[5]) + 'deg);"></div>';
+
+        htmlMarker.addEventListener('click', () => {
+          Actions.displayDetail(element);
+          if (this.props.mobile)
+            Actions.leftActive(false);
+        });
+
+        var marker = new mapboxgl.Marker(htmlMarker);
+        marker.setLngLat([placeSplited[2], placeSplited[1]]);
+        marker.addTo(this.mymap);
+      }
+    });
   }
   render() {
-    var loc = [-122.39851786165565, 37.78736425435588];
-    var { viewport } = this.state;
-    return <MapGL onChangeViewport={this._onChangeViewport} {...viewport} mapboxApiAccessToken={'pk.eyJ1IjoiYXJndWVsYmVub2l0IiwiYSI6ImNpczN0aTRpbjAwMWQyb3FkM3d4d3dweWwifQ.TuZpfqS-HyuaUzbe1fIiTg'} >
-      <HTMLOverlay width={viewport.width} height={viewport.height}/>
-    </ MapGL>;
+    const { displayDetail } = this.props;
+    return <div className={displayDetail ? 'blur' : ''} id="map-container">
+      <div style={{ height: '100%' }} id="map" />
+    </div>;
   }
 }
 
 MapContainer.propTypes = {
-  viewport: PropTypes.any,
-  mapPosition: PropTypes.array
+  displayDetail: PropTypes.any,
+  hoverId: PropTypes.any,
+  detail: PropTypes.any,
+  place: PropTypes.object,
+  allId: PropTypes.array,
+  mapPosition: PropTypes.array,
+  mobile: PropTypes.bool
 };
 
 export default MapContainer;

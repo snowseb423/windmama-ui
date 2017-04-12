f1f49fe5d07d6312ebf1e94750087a9eefb03224
BenoitArguel <benoit.arguel@gmail.com>
2017-04-12 11:33:08 +0200

    refactor: remove all badListener on header.jsx

--- app/js/header.jsx
+++ app/js/header.jsx
@@ -1,21 +1,42 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { Actions } from './store/actions.js';
 
-
-function Header(props) {
-  return <div>
-    <div id="header" className="elements-ui-absolute">
-      <div className={props.leftActive ? 'container-left-menu button clicked' : 'container-left-menu button'} onClick={() => Actions.leftActivation()}>
-        <i className="fa fa-arrow-down" aria-hidden="true" />
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      right: false,
+      left: false
+    };
+  }
+  shouldComponentUpdate(nextProps) {
+    if (nextProps.rightActive !== this.state.right || nextProps.leftActive !== this.state.left )
+      return true;
+    else
+      return false;
+  }
+  componentDidUpdate(prevProps) {
+    this.setState({
+      right: prevProps.rightActive,
+      left: prevProps.leftActive
+    });
+  }
+  render() {
+    console.log('rerender');
+    return <div>
+      <div id="header" className="elements-ui-absolute">
+        <div className={this.state.left ? 'container-left-menu button clicked' : 'container-left-menu button'} onClick={() => Actions.leftActivation()}>
+          <i className="fa fa-arrow-down" aria-hidden="true" />
+        </div>
+        <h1 style={{cursor: 'pointer'}} onClick={() => Actions.displayDetail(false)}>WindMama.fr</h1>
+        <div className={this.state.right ? 'container-right-menu button clicked' : 'container-right-menu button'} onClick={() => Actions.rightActivation()}>
+          <div/>
+          <div/>
+        </div>
       </div>
-      <h1 style={{cursor: 'pointer'}} onClick={() => Actions.displayDetail(false)}>WindMama.fr</h1>
-      <div className={props.rightActive ? 'container-right-menu button clicked' : 'container-right-menu button'} onClick={() => Actions.rightActivation()}>
-        <div/>
-        <div/>
-      </div>
-    </div>
-    <div style={{width: '100%', height: '60px'}} />
-  </div>;
+      <div style={{width: '100%', height: '60px'}} />
+    </div>;
+  }
 }
 
 Header.propTypes = {

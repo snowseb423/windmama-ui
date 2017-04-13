316cbbf0f1dea6140848f6b761421cdf04a78596
BenoitArguel <benoit.arguel@gmail.com>
2017-04-13 10:57:34 +0200

    refactor(work in progress): change containerWidget lifecycle

--- app/js/containerWidget.jsx
+++ app/js/containerWidget.jsx
@@ -1,40 +1,76 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import InfoWidget from './infoWidget.jsx';
 import Last2HoursWidget from './last2HoursWidget.jsx';
 import Last24HoursWidget from './last24HoursWidget.jsx';
 
-function ContainerWidget(props) {
-  const { displayDetail, detail, onePlace, leftActive, rightActive, mobile, viewportWidth, viewportHeight } = props;
-  var content = <div>
-    <InfoWidget place={onePlace} />
-    <Last2HoursWidget detail={detail[displayDetail]} />
-    <Last24HoursWidget mobile={mobile} detail={detail[displayDetail]} />
-  </div>;
-
-  var widthContainer;
-  var marginLeftContainer;
-  if (!leftActive && !rightActive && !mobile) {
-    widthContainer = viewportWidth;
-    marginLeftContainer = 0;
-  } else if (leftActive && !mobile) {
-    widthContainer = (viewportWidth - 260) + 'px';
-    marginLeftContainer = 260;
-  } else if (rightActive && !mobile) {
-    widthContainer = (viewportWidth - 260) + 'px';
-    marginLeftContainer = 0;
+class ContainerWidget extends Component {
+  constructor(props) {
+    super(props);
+    const { displayDetail, rightActive, leftActive, viewportWidth, viewportHeight } = this.props;
+    this.state = {
+      displayDetail,
+      rightActive,
+      leftActive,
+      viewportWidth,
+      viewportHeight
+    };
   }
+  shouldComponentUpdate(nextProps) {
+    console.log(nextProps);
+    if (nextProps.displayDetail !== this.state.displayDetail ||
+      nextProps.rightActive !== this.state.rightActive ||
+      nextProps.leftActive !== this.state.leftActive ||
+      nextProps.viewportWidth !== this.state.viewportWidth ||
+      nextProps.viewportHeight !== this.state.viewportHeight)
+      return true;
+    else if (nextProps.idUpdate === this.state.displayDetail)
+      return true;
+    else
+      return false;
+  }
+  componentDidUpdate(prevProps) {
+    this.setState({
+      displayDetail: prevProps.displayDetail,
+      leftActive: prevProps.leftActive,
+      rightActive: prevProps.rightActive,
+      viewportHeight: prevProps.viewportHeight,
+      viewportWidth: prevProps.viewportWidth
+    });
+  }
+  render() {
+    const { displayDetail, rightActive, leftActive, viewportWidth, viewportHeight } = this.state;
+    const { detail, onePlace, mobile } = this.props;
+    console.log(this.props.idUpdate + ' ' + displayDetail);
+    var content = <div>
+      <InfoWidget place={onePlace} />
+      <Last2HoursWidget detail={detail[displayDetail]} />
+      <Last24HoursWidget mobile={mobile} detail={detail[displayDetail]} />
+    </div>;
+    var widthContainer;
+    var marginLeftContainer;
+    if (!leftActive && !rightActive && !mobile) {
+      widthContainer = viewportWidth;
+      marginLeftContainer = 0;
+    } else if (leftActive && !mobile) {
+      widthContainer = (viewportWidth - 260) + 'px';
+      marginLeftContainer = 260;
+    } else if (rightActive && !mobile) {
+      widthContainer = (viewportWidth - 260) + 'px';
+      marginLeftContainer = 0;
+    }
 
-  var heightCoverWidget = {
-    height: viewportHeight - 60 + 'px',
-    width: widthContainer,
-    marginLeft: marginLeftContainer
-  };
+    var heightCoverWidget = {
+      height: viewportHeight - 60 + 'px',
+      width: widthContainer,
+      marginLeft: marginLeftContainer
+    };
 
-  return <div id="cover-widgets" style={heightCoverWidget} className={displayDetail ? 'active' : ''}>
-    <div className="container-widgets" id="container-widgets" >
-      {displayDetail ? content : ''}
-    </div>
-  </div>;
+    return <div id="cover-widgets" style={heightCoverWidget} className={displayDetail ? 'active' : ''}>
+      <div className="container-widgets" id="container-widgets" >
+        {displayDetail ? content : ''}
+      </div>
+    </div>;
+  }
 }
 
 ContainerWidget.propTypes = {

a53c871a5ff45b76157bc58d2c208edd08e9692c
BenoitArguel <benoit.arguel@gmail.com>
2017-06-19 23:09:26 +0200

    feat: dynamic date (work in progress)

--- app/js/infoWidget.jsx
+++ app/js/infoWidget.jsx
@@ -1,37 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Actions } from './store/actions.js';
 import moment from 'moment';
 import 'moment/locale/fr';
+moment.locale('fr');
 
-function InfoWidget(props) {
-  moment.locale('fr');
-  const { place, oneDetail } = props;
-  const styleCityName = {
-    fontFamily: 'Abel',
-    fontWeight: 'bolder',
-    fontSize: '18px',
-    padding: '0 10px'
-  };
-  if(place) {
-    var info = <div style={styleCityName}>
-      {place[4] + ', '}<a href={'http://pioupiou.fr/fr/' + place[0]} target="_blan">Pioupiou_n°{place[0]}</a><br/>
-      {'Dernier relevé à ' + moment(oneDetail[0][1], moment.ISO_8601).format('HH:mm') + ' le ' + moment(oneDetail[0][1], moment.ISO_8601).format('D MMMM') + '.'}
-    </div>;
-    if (place[4].indexOf('Unnamed Road') >= 0) {
-      info = <div style={styleCityName}>
-        {place[4].substring(13) + ', '}<a href={'http://pioupiou.fr/fr/' + place[0]} target="_blan">Pioupiou_n°{place[0]}</a><br/>
-        {'Dernier relevé à ' + moment(oneDetail[0][1], moment.ISO_8601).format('HH:mm') + ' le ' + moment(oneDetail[0][1], moment.ISO_8601).format('D MMMM') + '.'}
-      </div>;
-    }
+// moment.utc(moment()
+//       .diff(moment(moment(this.props.oneDetail[0][1], moment.ISO_8601).format())))
+//       .format('HH:mm:ss')
+
+class InfoWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.updateTime = this.updateTime.bind(this);
+    this.state = {
+      last: this.props.oneDetail[0][1],
+      diff: moment.utc(moment().format()
+                  .diff(moment(moment(this.props.oneDetail[0][1], moment.ISO_8601).format())))
+                  .format('HH:mm:ss')
+    };
   }
-  return <div className="widget" style={{ padding: '10px 0', background: 'rgba(255, 255, 255, 0.25)', color: '#fff'}}>
-    <div style={{ display: 'inline-block', width: '100%' }}>
-      <button className="button" style={{ opacity: '0.4', float: 'left', marginLeft: '10px'}} onClick={() => alert('Cette fonction sera bientôt disponible')}><i className="fa fa-heart-o" /> favoris</button>
-      <button className="button"  style={{ float: 'right', marginRight: '10px'}} onClick={() => Actions.displayDetail(false)}><i className="fa fa-times" /></button>
-    </div>
-    {info}
-  </div>;
+  componentWillMount() {
+    this.updateTime();
+  }
+  shouldComponentUpdate() {
+    return true;
+  }
+  updateTime() {
+    // setInterval(() => {
+    //   this.setState({
+    //     diff: moment(this.state.diff).add(1, 'seconds')
+    //   });
+    // }, 1000);
+  }
+  render() {
+    const { place } = this.props;
+    const styleCityName = {
+      fontFamily: 'Abel',
+      fontSize: '18px',
+      padding: '0 10px'
+    };
+    return <div className="widget" style={{ padding: '10px 0', background: 'rgba(255, 255, 255, 0.25)', color: '#fff'}}>
+      <div style={{ display: 'inline-block', width: '100%' }}>
+        <button className="button" style={{ opacity: '0.4', float: 'left', marginLeft: '10px'}} onClick={() => alert('Cette fonction sera bientôt disponible')}><i className="fa fa-heart-o" /> favoris</button>
+        <button className="button"  style={{ float: 'right', marginRight: '10px'}} onClick={() => Actions.displayDetail(false)}><i className="fa fa-times" /></button>
+      </div>
+      <div style={styleCityName}>
+        {place[4] + ', '}<a href={'http://pioupiou.fr/fr/' + place[0]} target="_blan">Pioupiou_n°{place[0]}</a><br/>
+        {'Dernier relevé à ' + moment(this.state.last).format('HH:mm') + ' le ' + moment(this.state.last).format('D MMMM') + '. Il y a '+ moment(this.state.diff).format('HH:mm:ss')}
+      </div>
+    </div>;
+  }
 }
 
 InfoWidget.propTypes = {
